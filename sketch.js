cb79d0229f0431203936e2f3029fead1a0ef84aa
Adilmojangos8246 <67742933+Adilmojangos8246@users.noreply.github.com>
2021-01-06 22:01:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//Create variables here
+var dog,happydog,database,foodS,FoodStock
+function preload()
+{
+  //load images here
+  
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+  background(46,  139, 87)
+  drawSprites();
+  //add styles here
+
+}
+
+
+
