aa7964392cecded6b1b4d7b51c1ab07dc5740ca1
Adilmojangos8246 <67742933+Adilmojangos8246@users.noreply.github.com>
2021-01-06 22:27:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,21 +3,48 @@ var dog,happydog,database,foodS,FoodStock
 function preload()
 {
   //load images here
-  
+  dog = loadImage("doglmg.png");
+  happydog = loadImage("doglmg1.png");
 }
 
 function setup() {
-	createCanvas(800, 700);
-  
+	createCanvas(500, 500);
+  dog = createSprite(200,200,20,50)
+
+  FoodStock =database.ref("food");
+  FoodStock.on("value",readpStock);
 }
 
 
 function draw() {  
   background(46,  139, 87)
+
+   if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(doglmg1);
+   } 
+
+   function readpStock(data){
+    foodS = data.val();
+  }
+  
+  function writeStock(x){
+    if(x<=0){
+      x = 0;
+    }else{
+      x=x-1;
+    }
+    database.ref('/').update({
+      food:x
+    })
+  }
+  
+
   drawSprites();
   //add styles here
 
 }
+
 
 
 
